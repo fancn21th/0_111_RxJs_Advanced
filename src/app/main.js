354d753dc62c5d70750e1032021a78c702fd77ba
fancn <fanyj@yikeer.com>
2019-09-19 22:46:24 +0800

    feature: create a observable

--- src/app/main.js
+++ src/app/main.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+var bar = Rx.Observable.create(function(observer) {
+  observer.next("foo");
+  observer.next("baz");
+  setInterval(function() {
+    observer.next("bar");
+  }, 1000);
+});
+
+bar.subscribe(function(val) {
+  console.log(val);
+});
