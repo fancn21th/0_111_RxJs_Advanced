ad25191ace0ee29be864c56fd5f761cc26f4e3d5
fancn <fanyj@yikeer.com>
2019-09-22 12:19:10 +0800

    feature: mapTo

--- src/app/main.js
+++ src/app/main.js
@@ -1,26 +1,6 @@
 var bar = Rx.Observable.interval(1000);
 
-var calculate = function(calculateFn) {
-  var source = this;
-  var result = Rx.Observable.create(function(observer) {
-    source.subscribe(
-      function(x) {
-        observer.next(calculateFn(x));
-      },
-      function(err) {
-        observer.error(err);
-      },
-      function() {
-        observer.complete();
-      }
-    );
-  });
-  return result;
-};
-
-Rx.Observable.prototype.calculate = calculate;
-
-var foo = bar.calculate(x => x * 2);
+var foo = bar.mapTo(10);
 
 foo.subscribe(
   function(val) {
