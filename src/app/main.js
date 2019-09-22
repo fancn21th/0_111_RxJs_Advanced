d85a44ce6257cdc4c40fb06b962de130b6f6449f
fancn <fanyj@yikeer.com>
2019-09-22 12:10:11 +0800

    feature: operator calculate

--- src/app/main.js
+++ src/app/main.js
@@ -1,11 +1,11 @@
-var bar = Rx.Observable.of(1, 2, 3);
+var bar = Rx.Observable.interval(1000);
 
-var multiplyByTen = function() {
+var calculate = function(calculateFn) {
   var source = this;
   var result = Rx.Observable.create(function(observer) {
     source.subscribe(
       function(x) {
-        observer.next(x * 10);
+        observer.next(calculateFn(x));
       },
       function(err) {
         observer.error(err);
@@ -18,43 +18,11 @@ var multiplyByTen = function() {
   return result;
 };
 
-var multiplyBy = function(multiplier) {
-  var source = this;
-  var result = Rx.Observable.create(function(observer) {
-    source.subscribe(
-      function(x) {
-        observer.next(x * multiplier);
-      },
-      function(err) {
-        observer.error(err);
-      },
-      function() {
-        observer.complete();
-      }
-    );
-  });
-  return result;
-};
+Rx.Observable.prototype.calculate = calculate;
 
-Rx.Observable.prototype.multiplyByTen = multiplyByTen;
-Rx.Observable.prototype.multiplyBy = multiplyBy;
-
-var foo = bar.multiplyByTen();
-var baz = bar.multiplyBy(100);
+var foo = bar.calculate(x => x * 2);
 
 foo.subscribe(
-  function(val) {
-    console.log("next", val);
-  },
-  function(err) {
-    console.log("error", err);
-  },
-  function() {
-    console.log("done");
-  }
-);
-
-baz.subscribe(
   function(val) {
     console.log("next", val);
   },
