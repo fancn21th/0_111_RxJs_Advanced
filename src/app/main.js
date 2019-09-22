769f87b299ededeef95d473ea38c702150fcbf9d
fancn <fanyj@yikeer.com>
2019-09-22 19:54:48 +0800

    feature: combineLatest

--- src/app/main.js
+++ src/app/main.js
@@ -1,7 +1,15 @@
 var bar = Rx.Observable.interval(500).take(5);
-var more = Rx.Observable.of(5, 6, 7, 8, 9);
+var baz = Rx.Observable.interval(300).take(5);
 
-var foo = bar.concat(more).startWith("ready ... go!");
+/* 
+----0----1----2----3----4     (bar)
+--0--1--2--3--4               (baz)
+  combineLatest(bar, baz, (x, y) => x + y
+
+----01--23-4--56--7-----8
+*/
+
+var foo = Rx.Observable.combineLatest(bar, baz, (x, y) => x + y);
 
 foo.subscribe(
   function(val) {
