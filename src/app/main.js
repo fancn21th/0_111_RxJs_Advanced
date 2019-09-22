50bb20278d88967081d0825d53e078cf6f0df97e
fancn <fanyj@yikeer.com>
2019-09-22 12:40:43 +0800

    feature: operator do

--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,9 @@
-var bar = Rx.Observable.interval(1000);
+var bar = Rx.Observable.interval(1000).take(4);
 
-var foo = bar.mapTo(10);
+var foo = bar
+  .do(x => console.log("before", x))
+  .map(x => x * 2)
+  .do(x => console.log("after", x));
 
 foo.subscribe(
   function(val) {
