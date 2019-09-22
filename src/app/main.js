f344c4578497b27c37d104fcead94a21cfd10fa4
fancn <fanyj@yikeer.com>
2019-09-22 21:46:11 +0800

    feature: operator merge

--- src/app/main.js
+++ src/app/main.js
@@ -6,10 +6,10 @@ var baz = Rx.Observable.interval(300).take(5);
 --0--1--2--3--4               (baz)
   combineLatest(bar, baz, (x, y) => x + y
 
-----01--23-4--56--7-----8
+--0-01--21-3-24----3----4
 */
 
-var foo = Rx.Observable.combineLatest(bar, baz, (x, y) => x + y);
+var foo = Rx.Observable.merge(bar, baz);
 
 foo.subscribe(
   function(val) {
