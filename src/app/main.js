c7d6155441123fee1b8b1284ab69316d4338ad28
alex.fan <alex.fan@okchem.com>
2019-09-30 14:56:18 +0800

    feature: map the built-in op for the same purpose

--- src/app/main.js
+++ src/app/main.js
@@ -21,8 +21,21 @@ var calculate = function(calculateFn) {
 Rx.Observable.prototype.calculate = calculate;
 
 var foo = bar.calculate(x => x * 2);
+var baz = bar.map(x => x * 2);
 
 foo.subscribe(
+  function(val) {
+    console.log("next", val);
+  },
+  function(err) {
+    console.log("error", err);
+  },
+  function() {
+    console.log("done");
+  }
+);
+
+baz.subscribe(
   function(val) {
     console.log("next", val);
   },
