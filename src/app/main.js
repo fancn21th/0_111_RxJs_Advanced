a0bdbf50cf6e81304e90317baa181bbf95fbff02
fancn <fanyj@yikeer.com>
2019-10-03 15:04:59 +0800

    feature: operator scan

--- src/app/main.js
+++ src/app/main.js
@@ -2,15 +2,17 @@ var bar = Rx.Observable.of("H", "e", "l", "l", "o");
 var baz = Rx.Observable.interval(300).take(5);
 
 /*
-  (hello|)                  
-  ---0---1---2---3---4|      (bar)
-    zip((x,y) => x)
-  ---h---e---l---l---o|      (baz)
-  */
+(hello|)                          (foo)
+-----0-----1-----2-----3-----4|   (bar)
+       zip((x,y) => x)
+-----h-----e-----l-----l-----o|
+  scan((acc, x) => acc+x, '')
+-----h-----(he)--(hel)-(hell)(hello|)
+*/
 
 // c stands for character
 // n stands for number
-var foo = bar.zip(baz, (x, y) => x);
+var foo = bar.zip(baz, (x, y) => x).scan((acc, val) => acc + val, "");
 
 foo.subscribe(
   function(val) {
